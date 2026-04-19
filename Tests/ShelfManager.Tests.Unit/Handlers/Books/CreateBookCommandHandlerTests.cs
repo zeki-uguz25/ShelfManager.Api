a8aa0f71fd1854884a1ea77ee9f33eaca39ac22f
Zeki Uğuz <zekiuguz41gs156@gmail.com>
2026-04-19 15:39:00 +0300

    fixed(2).

--- Tests/ShelfManager.Tests.Unit/Handlers/Books/CreateBookCommandHandlerTests.cs
+++ Tests/ShelfManager.Tests.Unit/Handlers/Books/CreateBookCommandHandlerTests.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShelfManager.Tests.Unit.Handlers.Books
+namespace ShelfManager.Tests.Unit.Handlers.Books//
 {
     public class CreateBookCommandHandlerTests
     {
